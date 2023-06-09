c6cfdde4d122d89b7302563e7f0ad318fc515329
Eric Avdey <eiri@eiri.ca>
2023-06-09 10:49:27 -0300

    feat: sync state for joined players

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -16,7 +16,7 @@ const props = defineProps({
 const { card } = toRefs(props);
 
 const store = useGameStore()
-const { captainView } = storeToRefs(store)
+const { subscribed, captainView } = storeToRefs(store)
 const { open } = store
 
 const cardClass = computed(() => {
@@ -33,7 +33,7 @@ const cardClass = computed(() => {
     :class="[cardClass, {animate__flipInY: !card.closed()}]"
     @click="open(card.idx)"
   >
-    <span>{{ card.word }}</span>
+    <span :class="{blur: !subscribed}">{{ card.word }}</span>
   </article>
 </template>
 
@@ -53,6 +53,10 @@ article {
 
 article:hover, .open {
   box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.3);
+}
+
+.blur {
+  filter: blur(8px);
 }
 
 /* black-open */
--- src/stores/game.js
+++ src/stores/game.js
@@ -6,6 +6,10 @@ import { words } from '../assets/words.js'
 import { CardState } from '../assets/states.js'
 
 export const useGameStore = defineStore('game', () => {
+  const uid = Math.random().toString(16).slice(2)
+  let online = []
+  console.log(`session uid ${uid}`)
+
   const supabase = inject('supabase')
   const channel = supabase.channel('game') // FIXME game-${room number}
   channel.on('broadcast', {event: '*'}, (msg) => {
@@ -18,11 +22,58 @@ export const useGameStore = defineStore('game', () => {
           gameKey.value = msg.payload.gameKey
           newGame()
         }
+        break;
+      case 'reqOnline':
+        channel.send({
+            type: 'broadcast',
+            event: 'ackOnline',
+            payload: {uid}
+        })
+        break;
+      case 'ackOnline':
+        online.push(msg.payload.uid)
+        break;
+      case 'reqState':
+        if (msg.payload.uid == uid) {
+          channel.send({
+              type: 'broadcast',
+              event: 'ackState',
+              payload: {
+                gameKey: gameKey.value,
+                state: board.value.map((c) => c.state)
+              }
+          })
+        }
+        break;
+      case 'ackState':
+        console.log('subscribed to broadcast')
+        console.log(msg.payload)
+        gameKey.value = msg.payload.gameKey
+        newGame()
+        subscribed.value = true
     }
   }).subscribe((status) => {
     if (status == 'SUBSCRIBED') {
-      console.log('subscribed to broadcast')
-      subscribed.value = true
+      channel.send({
+          type: 'broadcast',
+          event: 'reqOnline'
+      })
+      const wait = 600 // ms
+      setTimeout(() => {
+        if (online.length == 0) {
+          // assume this client is the first one
+          console.log('subscribed to broadcast')
+          subscribed.value = true
+        } else {
+          // FIXME: race here if top ack client quit after ansering,
+          // but that's ok for now
+          channel.send({
+              type: 'broadcast',
+              event: 'reqState',
+              payload: {uid: online[0]}
+          })
+        }
+      }, wait)
     }
   })
 
@@ -83,24 +134,35 @@ export const useGameStore = defineStore('game', () => {
     }
   }
 
-  const newGame = () => {
-    console.log(`new game ${gameKey.value}, board size ${boardSize.value}`)
-    rnd = prng_alea(gameKey.value)
-    const red = Math.round((boardSize.value - 1) / 3)
+  const newCards = (size) => {
+    const red = Math.round((size - 1) / 3)
     const blue = red - 1
-    const white = boardSize.value - red - blue - 1
+    const white = size - red - blue - 1
     let cards = [CardState.BlackClosed]
       .concat(Array(red).fill(CardState.RedClosed))
       .concat(Array(blue).fill(CardState.BlueClosed))
       .concat(Array(white).fill(CardState.WhiteClosed))
-    // FIXME: refactor to uniqueRandomWords(size) as this can have duplicates
-    const words = Array(boardSize.value).fill().map(randomWord)
 
     // Fisher-Yates shuffle
     for (let i = cards.length - 1; i > 0; i--) {
         const j = Math.floor(rnd() * (i + 1));
         [cards[i], cards[j]] = [cards[j], cards[i]];
     }
+
+    return {cards, score: {red, blue}}
+  }
+
+  const newWords = (size) => {
+    // FIXME: refactor to uniqueRandomWords(size) as this can have duplicates
+    return Array(size).fill().map(randomWord)
+  }
+
+
+  const newGame = () => {
+    console.log(`new game ${gameKey.value}, board size ${boardSize.value}`)
+    rnd = prng_alea(gameKey.value)
+    const { cards, score } = newCards(boardSize.value)
+    const words = newWords(boardSize.value)
 
     board.value = []
     for (let i in cards) {
@@ -112,7 +174,7 @@ export const useGameStore = defineStore('game', () => {
       })
     }
 
-    score.value = {red, blue}
+    score.value = score
     captainView.value = false
     gameOver.value = 'none'
     if (subscribed.value) {
@@ -123,6 +185,8 @@ export const useGameStore = defineStore('game', () => {
       })
     }
   }
+
+
 
   return { gameKey, boardSize, score, gameOver, captainView, subscribed,
     board, open, newGame, randomWord }
