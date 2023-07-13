d16a485fa942991a69222509a05a669cda9f47cc
Eric Avdey <eiri@eiri.ca>
2023-07-13 14:43:27 -0300

    refactor: port convertion util from python to node

---
+++ assets/convert.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+try {
+    const args = process.argv.slice(2)
+    if (args.length != 2) {
+        throw new Error('expectign exactly two arguments')
+    }
+    const fileIn = args[0]
+    const fileOut = args[1]
+    const data = fs.readFileSync(fileIn, 'utf8')
+    let words = data.split(/[ ,]+/).map(w => w.toLowerCase())
+    words.sort()
+    fs.writeFileSync(fileOut, JSON.stringify(words, null, 2))
+} catch (err) {
+    console.error(err)
+}
