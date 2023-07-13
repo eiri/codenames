fd6153d94badd99d059452057f8145a4c992bb1d
Eric Avdey <eiri@eiri.ca>
2023-07-12 21:10:50 -0300

    refactor: switch to custom uuidv4 for initial seed because of iOS Safari

--- src/stores/game.js
+++ src/stores/game.js
@@ -11,8 +11,10 @@ export const useGameStore = defineStore('game', () => {
   const boardSize = 25
   // https://colorkit.co/palette/ffadad-ffd6a5-fdffb6-caffbf-9bf6ff-a0c4ff-bdb2ff-ffc6ff/
   const palette = ["#ffadad","#ffd6a5","#fdffb6","#caffbf","#9bf6ff","#a0c4ff","#bdb2ff","#ffc6ff"]
-  // from https://stackoverflow.com/questions/18638900/javascript-crc32
+  // from https://stackoverflow.com/a/50579690
   const crc32=function(r){for(var a,o=[],c=0;c<256;c++){a=c;for(var f=0;f<8;f++)a=1&a?3988292384^a>>>1:a>>>1;o[c]=a}for(var n=-1,t=0;t<r.length;t++)n=n>>>8^o[255&(n^r.charCodeAt(t))];return(-1^n)>>>0};
+  // from https://stackoverflow.com/a/2117523
+  const uuidv4=function(){return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, function(c){return(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)})};
 
   const board = ref(Array(boardSize).fill().map(() => {
     return {
@@ -25,7 +27,7 @@ export const useGameStore = defineStore('game', () => {
   const players = ref({})
   const isCaptain = ref(false)
   const gameKey = ref('')
-  let rnd = prng_alea(self.crypto.randomUUID())
+  let rnd = prng_alea(uuidv4())
 
   const ablyAPIKey = import.meta.env.VITE_ABLY_API_KEY
 
