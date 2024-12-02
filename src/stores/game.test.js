27f7583c72f2b36f5d098639c1e9b0e1061d19a0
Eric Avdey <eiri@eiri.ca>
2024-12-02 18:07:22 -0400

    fix(test): drop broker mocking

--- src/stores/game.test.js
+++ src/stores/game.test.js
@@ -1,64 +1,23 @@
-import { beforeAll, afterAll, describe, it, expect, vi } from "vitest";
+import { beforeEach, it, describe, expect } from "vitest";
+import { createApp } from "vue";
 import { setActivePinia, createPinia } from "pinia";
+import rnd from "@/plugins/rnd";
 import { useGameStore } from "@/stores/game.js";
 
-vi.mock("ably", () => {
-  let data = {};
-  const Realtime = vi.fn();
-  Realtime.prototype.connect = vi.fn();
-  Realtime.prototype.connection = {
-    on: (name, fn) => {
-      if (name === "connected") {
-        fn();
-      }
-    },
-  };
-  Realtime.prototype.channels = {
-    get: vi.fn(() => {
-      return {
-        publish: (name, payload) => {
-          data = payload;
-        },
-        presence: {
-          subscribe: vi.fn(),
-          get: vi.fn(),
-        },
-        subscribe: (name, fn) => {
-          if (name === "nextGame") {
-            fn({ data });
-          }
-        },
-      };
-    }),
-  };
-  return {
-    default: { Realtime },
-  };
-});
-
 describe("Game Store", () => {
-  beforeAll(async () => {
-    localStorage.setItem("password", "fakefake");
-    localStorage.setItem("username", "test");
-    localStorage.setItem("room", "test");
-    vi.spyOn(console, "assert").mockImplementation(() => {});
-    vi.spyOn(console, "debug").mockImplementation(() => {});
-    setActivePinia(createPinia());
-  });
+  const app = createApp({});
 
-  afterAll(() => {
-    vi.restoreAllMocks();
+  beforeEach(() => {
+    const pinia = createPinia();
+    app.use(rnd);
+    app.use(pinia);
+    setActivePinia(pinia);
   });
 
   it("Doesn't repeat words", () => {
     const store = useGameStore();
-
-    const connectSpy = vi.spyOn(store, "connect");
-    store.connect();
-    expect(connectSpy).toHaveBeenCalledTimes(1);
-
     for (let i = 1; i <= 12; i++) {
-      store.nextGame();
+      store.buildGame();
       let seen = [];
       for (const card of store.board) {
         expect(seen).not.toContain(card.word);
