1340a7594ff3cf729bc72eb5b02c8fe5c9b05323
Eric Avdey <eiri@eiri.ca>
2026-03-27 20:14:38 -0300

    chore: autoformat vue files

--- src/Game.vue
+++ src/Game.vue
@@ -11,40 +11,40 @@ const router = useRouter();
 const broker = inject<Broker>(brokerKey);
 
 onMounted(async () => {
-    if (localStorage.getItem("loggedIn")) {
-        console.log("this page was reloaded");
-        localStorage.removeItem("loggedIn");
-        router.push(`/`);
-        return;
-    }
+  if (localStorage.getItem("loggedIn")) {
+    console.log("this page was reloaded");
+    localStorage.removeItem("loggedIn");
+    router.push(`/`);
+    return;
+  }
 
-    console.debug(`Game: onMounted`);
-    localStorage.setItem("loggedIn", "true");
-    try {
-        await broker.connect();
-    } catch (error) {
-        console.error(error);
-        router.push(`/`);
-    }
+  console.debug(`Game: onMounted`);
+  localStorage.setItem("loggedIn", "true");
+  try {
+    await broker.connect();
+  } catch (error) {
+    console.error(error);
+    router.push(`/`);
+  }
 });
 
 onUnmounted(() => {
-    console.debug("Game: onUnmounted");
-    if (localStorage.getItem("loggedIn") === "true") {
-        broker.disconnect();
-        localStorage.removeItem("loggedIn");
-    }
+  console.debug("Game: onUnmounted");
+  if (localStorage.getItem("loggedIn") === "true") {
+    broker.disconnect();
+    localStorage.removeItem("loggedIn");
+  }
 });
 </script>
 
 <template>
-    <header class="p-4 shadow-md">
-        <Info />
-    </header>
-    <main class="p-4">
-        <Screen />
-    </main>
-    <footer class="pt-12">
-        <Controls />
-    </footer>
+  <header class="p-4 shadow-md">
+    <Info />
+  </header>
+  <main class="p-4">
+    <Screen />
+  </main>
+  <footer class="pt-12">
+    <Controls />
+  </footer>
 </template>
